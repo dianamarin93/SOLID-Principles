7163de4ecdaa4ef3860383c328360aed28c432e8
dianaM <diana.marin9324@gmail.com>
2023-02-01 19:55:45 +0200

    I added new ISP project.

---
+++ Interface Segregation Principle/HPLaserPrinter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_Principle
+{
+    class HPLaserPrinter : Printer, IPrintBothSidesTask, IPrintTask, IScanTask
+    {
+        public HPLaserPrinter(string brand, string color) : base(brand, color)
+        {
+        }
+
+        public void Print(Content content)
+        {
+            Console.WriteLine($"Printing {content}");
+        }
+      
+        public void Scan(Content content)
+        {
+            Console.WriteLine($"Scanning {content}");
+        }
+
+        public void PrintBothSides(Content content)
+        {
+            Console.WriteLine($"Printing both sides {content}");
+        }
+    }
+}
---
+++ Interface Segregation Principle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BrotherBusinessPrinter bp = new BrotherBusinessPrinter("Brother", "white");
+            bp.PrintBothSides(Content.Banner);
+            bp.Copy(Content.Pictures);
+
+            EpsonJetPrinter eps = new EpsonJetPrinter("Epson", "grey");
+            eps.Print(Content.Text);
+
+            CanonMultiFunctionPrinter cmp = new CanonMultiFunctionPrinter("Canon", "black");
+            cmp.Scan(Content.Pictures);
+            Console.ReadKey();
+        }
+    }
+}
