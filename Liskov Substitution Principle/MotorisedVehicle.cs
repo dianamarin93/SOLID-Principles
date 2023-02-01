fceb9cc1943d8925515d148e754000f696013ab9
dianaM <diana.marin9324@gmail.com>
2023-02-01 19:00:29 +0200

    I added new LSP project.

---
+++ Liskov Substitution Principle/MotorisedVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution_Principle
+{
+    class MotorisedVehicle : Vehicle
+    {
+        public Engine Engine { get; set; }
+
+        public MotorisedVehicle(string name, int speed, int maxPassengers, Engine engine) : base(name, speed, maxPassengers)
+        {
+            Engine = engine;
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Engine is starting!");
+        }
+    }
+}
---
+++ Liskov Substitution Principle/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution_Principle
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int MaxPassengers { get; set; }
+
+
+        public Vehicle(string name, int speed, int maxPassengers)
+        {
+            Name = name;
+            Speed = speed;
+            MaxPassengers = maxPassengers;
+            
+        }
+
+        public override string ToString()
+            => $"Name : {Name}, Speed: {Speed}, MaxPassengers: {MaxPassengers}";
+
+      
+    }
+}
